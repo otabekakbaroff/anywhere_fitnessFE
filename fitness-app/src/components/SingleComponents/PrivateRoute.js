c49d5ecd723c95d0b21bf4e7f640e326e630970d
Otabek Akbarov <Otabek.Akbaroff@gmail.com>
2020-09-25 15:22:42 -0400

    yarn upgrade

--- fitness-app/src/components/SingleComponents/PrivateRoute.js
+++ fitness-app/src/components/SingleComponents/PrivateRoute.js
@@ -1,16 +1,6 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import {Route, Redirect} from 'react-router-dom'
-import axiosWithAuth from './axiosWithAuth';
 const PrivateRoute = ({component:Component, ...rest}) =>{
-    useEffect(() => {
-        axiosWithAuth().get()
-    .then(response=>{
-        console.log(response);
-    })
-    .catch(error=>{
-        console.log(error);
-    }) 
-    },[]);
     return (
         <Route {...rest} render={()=>{
             if(localStorage.getItem('token')){
