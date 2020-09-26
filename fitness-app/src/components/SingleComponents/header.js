5939d9068b7a748cc699a5f3c5cfd2ca427220e4
Otabek Akbarov <Otabek.Akbaroff@gmail.com>
2020-09-26 10:08:22 -0400

    working on home page responsiveness

--- fitness-app/src/components/SingleComponents/header.js
+++ fitness-app/src/components/SingleComponents/header.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 function Navbar(){
+    function sayHello(){
+        let open = document.querySelectorAll(".nav .anchor-tags a")
+        if (open[0].className === 'close'){
+            for(let i = 0; i < open.length; i++){
+                open[i].className = 'open'
+            }
+        }else{
+            for(let i = 0; i < open.length; i++){
+                open[i].className = 'close'
+            }
+        }
+    }
     return(
-        <div className="Nav">
-            <Link to="/">Home</Link>
-            <Link to="/about">About</Link>
-            <Link to="/register/client">Sign Up</Link>
-            <Link to="/login/client">Login</Link>
+        <div>
+            <div className="nav">
+                <div className="anchor-tags">
+                    <Link to="/" className="close">Home</Link>
+                    <Link to="/about" className="close">About</Link>
+                    <Link to="/register/client" className="close">Sign Up</Link>
+                    <Link to="/login/client" className="close">Login</Link>
+                </div>
+            </div>
+            <div id="sandwich" onClick={sayHello}>
+                    <div id="layerOne"></div>
+                    <div id="layerTwo"></div>
+                    <div id="layerThree"></div>
+            </div>
         </div>
     )
 }
