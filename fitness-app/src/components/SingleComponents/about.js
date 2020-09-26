394b436a318298d00981d6dcdc5487d41b3ed141
Otabek Akbarov <Otabek.Akbaroff@gmail.com>
2020-09-26 12:11:39 -0400

    about page

--- fitness-app/src/components/SingleComponents/about.js
+++ fitness-app/src/components/SingleComponents/about.js
@@ -7,10 +7,10 @@ function Contact(){
             <section>
                 <h1>About Us</h1>
                 <p> Welcome to anywhere fitness, our app gives you freedom you need.</p>
-                <p>• You can choose your instructors •</p>
-                <p>• You can choose the time •</p>
-                <p>• You can choose the location •</p>
-                <p>• Simple and easy Registration process •</p>
+                <p>• You can choose your instructors </p>
+                <p>• You can choose the time </p>
+                <p>• You can choose the location </p>
+                <p>• Simple and easy Registration process </p>
                 <p> What are you waiting for? Join us today!</p>
             </section>
         </div>
